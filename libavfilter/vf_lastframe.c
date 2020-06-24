b75cdce71cea1a13f136dc48f82e028803bedaa3
Alicia <tl2180@nyu.edu>
2020-06-24 12:06:56 +0800

    lastframe: final

--- libavfilter/vf_lastframe.c
+++ libavfilter/vf_lastframe.c
@@ -20,7 +20,7 @@
 
 /**
  * @file
- * filter for selecting the last frame passes in the filterchain
+ * filter for selecting and holding the last frame passed in the filterchain
  */
 
 #include "libavutil/opt.h"
@@ -32,126 +32,24 @@
 
 typedef struct LastFrameContext {
     const AVClass *class;
-    AVFrame *last_frame;
-    int64_t pts;
-    int eof;
-    int frame_remaining;
+    int64_t duration;
+    int64_t frame_remaining;  // number of frames to hold
+    AVFrame *last_frame;  // store the last frame
 
+    int64_t pts;  // current pts of the newly freezed stream
+    int eof;  // whether approach to the end of the stream
 } LastFrameContext;
 
 #define OFFSET(x) offsetof(LastFrameContext, x)
+#define FLAGS AV_OPT_FLAG_FILTERING_PARAM|AV_OPT_FLAG_VIDEO_PARAM
 
 static const AVOption lastframe_options[] = {
+    { "duration", "set the duration to hold the frame (default is one second)",
+      OFFSET(duration), AV_OPT_TYPE_DURATION, {.i64=0}, 0, INT64_MAX, FLAGS},
     { NULL }
 };
 
 AVFILTER_DEFINE_CLASS(lastframe);
-
-// static int activate(AVFilterContext *ctx) {
-//     AVFilterLink *inlink = ctx->inputs[0];
-//     AVFilterLink *outlink = ctx->outputs[0];
-//     LastFrameContext *s = ctx->priv;
-//     AVFrame *frame = NULL;
-//     int ret, status;
-//     int64_t pts;
-
-//     FF_FILTER_FORWARD_STATUS_BACK(outlink, inlink);
-//     printf("ff_outlink_frame_wanted? %d\n", ff_outlink_frame_wanted(outlink));
-
-//     if (s->eof && s->frame_remaining > 0) {
-//       printf("endpoint2\n");
-//       s->frame_remaining--;
-//       frame = av_frame_clone(s->last_frame);
-//       frame->pts = s->pts;
-//       s->pts += av_rescale_q(1, av_inv_q(outlink->frame_rate), outlink->time_base);
-//       printf("\nremaining frame info: remaining %d, pts %d\n", s->frame_remaining, frame->pts);
-//       return ff_filter_frame(outlink, frame);
-//       // ff_outlink_set_status(outlink, AVERROR_EOF, pts);
-//       // return 0;
-//     } else if (s->eof) {
-//       printf("endpoint3\n");
-//       // pts = av_rescale_q(1, av_inv_q(outlink->frame_rate), outlink->time_base);
-//       printf("\nComplete\n", s->frame_remaining);
-//       s->last_frame = NULL;
-//       ff_outlink_set_status(outlink, AVERROR_EOF, s->pts);
-//       return 0;
-//     }
-
-//     if (!s->eof) { // 如果流还没有结束
-//         ret = ff_inlink_consume_frame(inlink, &frame);
-//         if (ret < 0) 
-//             return ret;
-//         if (ret > 0) {
-//             printf("\nframe info: pts %d\n", frame->pts);
-//             pts = av_rescale_q(1, av_inv_q(outlink->frame_rate), outlink->time_base);
-//             printf("\n-- single pts %d\n", pts);
-//             s->last_frame = av_frame_clone(frame);
-//             // av_frame_free(&frame);
-//             // return 0;
-//             return ff_filter_frame(outlink, frame);
-//         }
-
-//         if (ff_inlink_acknowledge_status(inlink, &status, &pts) && status == AVERROR_EOF) {
-//             s->eof = 1;
-//             s->pts = pts;
-//             // return ff_filter_frame(outlink, frame);
-//             // pts = av_rescale_q(1, av_inv_q(outlink->frame_rate), outlink->time_base);
-//             // printf("\n-- last frame pts %d\n", s->last_frame->pts);
-//             printf("\n\n\n\n\nthe stream ends: pts %d\n\n\n\n\n", pts);
-//             // ff_outlink_set_status(outlink, status, pts);
-//             return 0;
-//         }
-//     }
-    
-
-//     printf("endpoint8\n");
-//     // FF_FILTER_FORWARD_WANTED(outlink, inlink);
-//     // return FFERROR_NOT_READY;
-//     // FF_FILTER_FORWARD_WANTED(outlink, inlink);
-//     // printf("endpoint9\n");
-//     // return FFERROR_NOT_READY;
-//     return 0;
-// }
-
-
-
-//     if (!s->eof) { // 如果流还没有结束
-//         ret = ff_inlink_consume_frame(inlink, &frame);
-//         if (ret < 0) 
-//             return ret;
-//         if (ret > 0) {
-//             printf("\nframe info: pts %d\n", frame->pts);
-//             pts = av_rescale_q(1, av_inv_q(outlink->frame_rate), outlink->time_base);
-//             printf("\n-- single pts %d\n", pts);
-//             s->last_frame = av_frame_clone(frame);
-//             // av_frame_free(&frame);
-//             // return 0;
-//             return ff_filter_frame(outlink, frame);
-//         }
-
-//         if (ff_inlink_acknowledge_status(inlink, &status, &pts) && status == AVERROR_EOF) {
-//             s->eof = 1;
-//             s->pts = pts;
-//             // return ff_filter_frame(outlink, frame);
-//             // pts = av_rescale_q(1, av_inv_q(outlink->frame_rate), outlink->time_base);
-//             // printf("\n-- last frame pts %d\n", s->last_frame->pts);
-//             printf("\n\n\n\n\nthe stream ends: pts %d\n\n\n\n\n", pts);
-//             // ff_outlink_set_status(outlink, status, pts);
-//             return 0;
-//         }
-//     }
-    
-
-//     printf("endpoint8\n");
-//     // FF_FILTER_FORWARD_WANTED(outlink, inlink);
-//     // return FFERROR_NOT_READY;
-//     // FF_FILTER_FORWARD_WANTED(outlink, inlink);
-//     // printf("endpoint9\n");
-//     // return FFERROR_NOT_READY;
-//     return 0;
-// }
-
-
 
 static int activate(AVFilterContext *ctx) {
     AVFilterLink *inlink = ctx->inputs[0];
@@ -162,43 +60,33 @@ static int activate(AVFilterContext *ctx) {
     int64_t pts;
 
     FF_FILTER_FORWARD_STATUS_BACK(outlink, inlink);
-    printf("ff_outlink_frame_wanted? %d\n\n", ff_outlink_frame_wanted(outlink));
- 
+
+    // if the stream has not ended, read from it
     if (!s->eof) {
         ret = ff_inlink_consume_frame(inlink, &frame);
         if (ret < 0)
             return ret;
         if (ret > 0) {
-          printf("\nframe info: pts %d\n", frame->pts);
+          // if read the frame successfully, update the last_frame attribute
           s->last_frame = av_frame_clone(frame);
-          frame->pts = 0;
-          // return ff_filter_frame(outlink, frame);
           av_frame_free(&frame);
-          // return 0;
-        }
-    }
-    
-    // 如果流还没有结束，但是status change发生了，流结束了
-    if (!s->eof && ff_inlink_acknowledge_status(inlink, &status, &pts)) {
-        if (status == AVERROR_EOF) {
-            printf("\n\n\n\n\nthe stream ends: pts %d\n\n\n\n\n", pts);
-            printf("self: pts %d\n\n\n\n\n", s->pts);
-            s->eof = 1; // 更新说明结束了
-            // s->pts += pts; 
         }
     }
 
-    printf("\n\n-------- in_frame_count_out %d, out_frame_count_out %d \neof: %d, s->pts %d, s->frame_remaining %d\n",
-    inlink->frame_count_out, outlink->frame_count_out,s->eof, s->pts, s->frame_remaining);
-    // 如果视频流结束了，但是还需要加frame
+    // if a status change is detected, update the status
+    if (!s->eof && ff_inlink_acknowledge_status(inlink, &status, &pts)) {
+        if (status == AVERROR_EOF) {
+            s->eof = 1;
+        }
+    }
+
+    // if no frame remaining in the FIFO queue
     if (s->eof) {
-        // 如果结尾已经加完了，告诉说结束
+        // if all extra frames have been outputted, inform the status change
         if (!s->frame_remaining) {
-            printf("completed!\n");
-            ff_outlink_set_status(outlink, AVERROR_EOF, 64000);
+            ff_outlink_set_status(outlink, AVERROR_EOF, s->pts);
             return 0;
         } else {
-          printf("s->pts %d, s->frame_remaining %d\n", s->pts, s->frame_remaining);
           frame = av_frame_clone(s->last_frame);
           if (!frame)
               return AVERROR(ENOMEM);
@@ -209,18 +97,15 @@ static int activate(AVFilterContext *ctx) {
           return ff_filter_frame(outlink, frame);
         }
     }
-    // 如果视频流还没有结束，或者还有需要加的frames
-    if (!s->eof || s->frame_remaining > 0) 
+    // if the stream has not ended or there are frames left.
+    if (!s->eof || s->frame_remaining > 0)
         FF_FILTER_FORWARD_WANTED(outlink, inlink);
 
     return FFERROR_NOT_READY;
-
-
 }
 
-//currently we just support the most common YUV420, can add more if needed
-static int query_formats(AVFilterContext *ctx)
-{
+// currently we just support the most common YUV420, can add more if needed
+static int query_formats(AVFilterContext *ctx) {
     static const enum AVPixelFormat formats[] = {
         AV_PIX_FMT_RGB24,
         AV_PIX_FMT_YUV420P,
@@ -230,20 +115,22 @@ static int query_formats(AVFilterContext *ctx)
     return ff_set_common_formats(ctx, ff_make_format_list(formats));
 }
 
-static av_cold void uninit(AVFilterContext *ctx)
-{
+static av_cold void uninit(AVFilterContext *ctx) {
     LastFrameContext *s = ctx->priv;
 
     av_frame_free(&s->last_frame);
 }
 
-static int config_input(AVFilterLink *inlink)
-{
+static int config_input(AVFilterLink *inlink) {
     AVFilterContext *ctx = inlink->dst;
     LastFrameContext *s = ctx->priv;
 
-    s->frame_remaining = 50;
-
+    if (s->duration) {
+      s->frame_remaining = av_rescale_q(s->duration, inlink->frame_rate, av_inv_q(AV_TIME_BASE_Q));
+    } else {
+      // the default number of frame is the frame_rate
+      s->frame_remaining = av_rescale_q(AV_TIME_BASE, inlink->frame_rate, av_inv_q(AV_TIME_BASE_Q));
+    }
     return 0;
 }
 
@@ -266,7 +153,7 @@ static const AVFilterPad avfilter_vf_lastframe_outputs[] = {
 
 AVFilter ff_vf_lastframe = {
     .name          = "lastframe",
-    .description   = NULL_IF_CONFIG_SMALL("Select the last frame of a video to pass in output."),
+    .description   = NULL_IF_CONFIG_SMALL("Select and hold the last frame of a video to pass in output."),
     .priv_size     = sizeof(LastFrameContext),
     .priv_class    = &lastframe_class,
     .query_formats = query_formats,
